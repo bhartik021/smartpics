5bf601b94f1eb38e130d92426513441b54b95cd8
Bharti Kumari <bhartik021@gmail.com>
2023-04-03 15:43:16 +0530

    create main

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,6 +7,8 @@ import { Home, CreatePost } from './Pages';
 const App = () => {
   return (
     <BrowserRouter>
+
+    {/* Header */}
     <header className="w-full flex justify-between items-center bg-white sm:px-8 px-4 py-4 border-b border-b-[#e6ebf4]">
 
       <Link to="/">
@@ -17,6 +19,11 @@ const App = () => {
       <Link to="/create-post"
       className="font-inter font-medium bg-[#6469ff] text-white px-4 py-2 rounded-md">Create</Link>
     </header>
+
+    {/* Main */}
+    <main className="sm:p-8 px-4 py-8 w-full bg-[#f9fafe] min-h-[calc(100vh-73px)]">
+
+    </main>
     </BrowserRouter>
   )
 }
